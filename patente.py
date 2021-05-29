842fa8bf079a327c10d120e8a35f56af9fc9770e
alex baeza <alcebaeza@gmail.com>
2021-05-28 22:45:31 -0400

    4to commit

--- patente.py
+++ patente.py
@@ -1,10 +1,20 @@
-patente = str(input("ingrese patente "))
-listaPalabras = list(patente)
-palabra = str(input("ingrese letra o número "))
-contador = 0
-for i in listaPalabras:
-    contador = listaPalabras.count(palabra)
+opcion = 0
+print("El siguiente programa pide un numero de patente\n- ingresada la patente, luego se pide un número o letra ")
+print("como salida mostrará cuantas veces se repite en la patente el parámetro pedido, sea letra o número")
+while opcion < 2:
+    opcion = int(input("ingrese 1 para ingresar patente\nIngrese 2 para salir "))
+    if opcion == 1:
+        patente = str(input("ingrese patente "))
+        lista = list(patente)
+        cadena = str(input("ingrese letra o número "))
+        counter = 0
+        for i in lista:
+            counter = lista.count(cadena)
+        print(f"la letra ingresada {cadena} se repite en la patente {counter}")
+    elif opcion == 2:
+        break
 
-print(f"la letra ingresada {palabra} se repite en la patente {contador}")
+
+
 
 
--- prueba_array.py
+++ prueba_array.py
@@ -1,35 +1,37 @@
-tot = 0
 acu = 0
 mon = 0
 opcion = 0
-don = 0
-bis = 0
-chil = 0
+dona = 0
+biscocho = 0
+chilenito = 0
+start = True
 
-while opcion < 4:
+
+while start:
     try: 
         print("   Pastel|valor\n1) Dona -> $1200\n2) Biscocho -> $1500\n3) Chileno -> $1000")
         print("4) opción 4 es ver boleta")
         opcion = int(input("ingrese opción "))
-        if opcion < 1:
+        if opcion < 1 or opcion > 4:
             raise Exception("número negativo, debe ser entre 1 al 4 positivo")
         if opcion == 1:
             cantidad1 = int(input("¿Cuántos llevará "))
             mon = cantidad1 * 1200
             acu += mon
-            don += cantidad1
+            dona += cantidad1
         elif opcion == 2:
-            cantidad2 = int(input("¿Cuántos llevará "))
+            cantidad2 = int(input("¿Cuántos llevará? "))
             mon = cantidad2 * 1500
             acu += mon 
-            bis += cantidad2
+            biscocho += cantidad2
         elif opcion == 3:
-            cantidad3 = int(input("¿Cuántos llevará "))
+            cantidad3 = int(input("¿Cuántos llevará? "))
             mon = cantidad3 * 1000
             acu += mon
-            chil += cantidad3
+            chilenito += cantidad3
         elif opcion == 4:
-            print(f"compró\n{don} donas\n{bis} biscochos\n{chil} chilenos\nSu monto total es:{acu}")
+            print(f"compró\n{dona} donas\n{biscocho} biscochos\n{chilenito} chilenos\nSu monto total es:{acu}")
+            start =False
 
     except:
-        print("los valores deben ser números enteros") 
+        print("los valores deben ser números enteros positivos del 1 al 4") 
